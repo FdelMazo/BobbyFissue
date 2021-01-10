c29f7bf6d669d22e8e6578518b0a6ad0412781ce
FdelMazo <fdelmazo@fi.uba.ar>
2021-01-10 12:00:38 -0300

    Add endgame header and invalid move reaction

--- scripts/chessmove.py
+++ scripts/chessmove.py
@@ -5,12 +5,22 @@ fen = sys.argv[1]
 move = sys.argv[2]
 
 board = chess.Board(fen)
-board.push_san(move)
+invalid_move = False
+
+
+try:
+	board.push_san(move)
+except ValueError:
+	invalid_move = True
+
 res = board.fen()
+
+if invalid_move:
+	res = f"{res} /"
 
 if board.is_checkmate():
 	res = f"{res} +"
 elif board.is_stalemate() or board.is_insufficient_material():
 	res = f"{res} -"
 
-print(board.fen())
+print(res)
