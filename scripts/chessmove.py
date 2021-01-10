fc65aba8a8b9b9ec012d53420fe4727dc4054c25
FdelMazo <fdelmazo@fi.uba.ar>
2021-01-10 12:28:31 -0300

    Prevent edge case of invalid move and cehckmate

--- scripts/chessmove.py
+++ scripts/chessmove.py
@@ -21,8 +21,7 @@ res = board.fen()
 
 if invalid_move:
 	res = f"{res} I"
-
-if board.is_checkmate():
+elif board.is_checkmate():
 	res = f"{res} W"
 elif board.is_stalemate() or board.is_insufficient_material():
 	res = f"{res} D"
