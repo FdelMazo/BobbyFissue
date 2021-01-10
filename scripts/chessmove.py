fba972ceeb977fc0a08d9069ac2c842db1d09c6b
FdelMazo <fdelmazo@fi.uba.ar>
2021-01-10 12:11:59 -0300

    Allow chessmove.py to parse custom_fen strs

--- scripts/chessmove.py
+++ scripts/chessmove.py
@@ -1,12 +1,16 @@
 import sys
 import chess
 
-fen = sys.argv[1]
+fen = sys.argv[1].strip()
 move = sys.argv[2]
+
+# Remove the 'custom fen' attribute (a last letter, I W or D which indicates InvalidMove, Win or Draw)
+attrs = fen.split(' ')
+if len(attrs) > 6:
+	fen = ' '.join(attrs[:6])
 
 board = chess.Board(fen)
 invalid_move = False
-
 
 try:
 	board.push_san(move)
