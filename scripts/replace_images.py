84bb25d16950f95097c8638ca667156ef78da24f
FdelMazo <fdelmazo@fi.uba.ar>
2021-01-09 21:19:39 -0300

    Change replace_images to receive from stdin

--- scripts/replace_images.py
+++ scripts/replace_images.py
@@ -1,7 +1,7 @@
 import re
 import sys
 
-board = sys.argv[1]
+board = sys.stdin.read()
 board = re.sub(r"([rRnNbBqQkKpP])", rf"![](https://raw.githubusercontent.com/FdelMazo/BobbyFissue/master/pieces/\1.png)", board)
 
 print(board)
