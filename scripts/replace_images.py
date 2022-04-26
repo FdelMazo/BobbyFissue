1d281be6331a72642562112db986d96a9341db74
FdelMazo <FdelMazo@fi.uba.ar>
2022-04-26 16:16:33 -0300

    Add invisible picture to empty squares

--- scripts/replace_images.py
+++ scripts/replace_images.py
@@ -4,7 +4,7 @@ import sys
 wholeboard = sys.stdin.read()
 header, board = wholeboard.split('**8**')
 board = re.sub(r"([rRnNbBqQkKpP])", rf"![](https://raw.githubusercontent.com/FdelMazo/BobbyFissue/master/pieces/\1.png)", board)
-
+board = re.sub(r" ", rf"![](https://raw.githubusercontent.com/FdelMazo/BobbyFissue/master/pieces/empty.png)", board)
 result = '**8**'.join([header, board])
 
 print(result)
